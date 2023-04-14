1036c89ef492ba39db96b53ff07ed9b7cf982421
reid <reidbras@gmail.com>
2023-04-14 17:23:41 -0500

    Adding initial commit for server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // Complete Events Exercise
+import * as http from "http"; //ES 6
+// const http = require("http");
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.write("Working");
+    res.end();
+});
+
+
+server.listen(port, () => { console.log(`Server listening on port ${port}`)});
