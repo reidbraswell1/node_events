f80ca6f7b7065e3abd6f483601bf063ecc5b9bae
reid <reidbras@gmail.com>
2023-04-14 21:37:37 -0500

    Capture post body

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 
     const { url, method } = req;
+    const chunks = [];
 
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.write("Working");
-    res.end();
+    req.on("data", (chunk) => { chunks.push(chunk) })
+
+    if (url == "/newsletter_sign_up" && method == "POST") {
+
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "text/html" })
+        res.write("<h1>404 Page Not Found<h1>");
+        res.end();
+    }
 });
 
 
-server.listen(port, () => { console.log(`Server listening on port ${port}`)});
+server.listen(port, () => { console.log(`Server listening on port ${port}`) });
